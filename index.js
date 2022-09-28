b8bd8f6b99a117f47d61fffcfff57f0a57ba1438
Christopher DiStasio <cadistasio@gmail.com>
2022-09-28 14:47:01 -0500

    Completes first exercise

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-let monthlyProfitLabels = [ 'January: 5', 'February: -1', 'March: 12' ]
-let monthList = document.querySelector('.month-list')
+// let monthlyProfitLabels = [ 'January: 5', 'February: -1', 'March: 12' ]
+// let monthList = document.querySelector('.month-list')
 
-for(let i = 0; i < monthlyProfitLabels.length; i++){
-    let label = monthlyProfitLabels[i]
-    let monthLi = document.createElement('li')
-    monthLi.textContent = label
-    monthList.append(monthLi)
-}
+// for(let i = 0; i < monthlyProfitLabels.length; i++){
+//     let label = monthlyProfitLabels[i]
+//     let monthLi = document.createElement('li')
+//     monthLi.textContent = label
+//     monthList.append(monthLi)
+// }
+
+let monthlyProfitLabels = [ 'January: 5', 'February: -1', 'March: 12' ];
+let monthList = document.querySelector(".month-list");
+
+let firstQuarter = monthlyProfitLabels.forEach(function (label) {
+	let monthLi = document.createElement("li");
+	monthLi.textContent = label;
+	monthList.append(monthLi);
+});
+
